5620197ae9193035df6d8b574141a6f521445850
Renato <renato@Renato-MacBook-Pro.local>
2020-11-04 02:27:46 +0200

    exercicio2

--- D01/EX02/index.js
+++ D01/EX02/index.js
@@ -1,6 +1,16 @@
 function isBiggerThan() {
-  //your code
-}
+  let chair = 2 ;
+    if (chair < 4) {
+      console.log("the number ${chair} is less  to 4");
+    }
+    if (chair > 4) {
+      console.log("The number ${chair} is bigger than 4")
+    }
+    else {
+      console.log("the number ${chair} is equal to 4")
+    }
+
+  }
 
 // Do not remove or change this line, or the tests won't work
 export { isBiggerThan };
