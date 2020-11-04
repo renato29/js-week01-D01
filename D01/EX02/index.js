5e7ae4e0ac68c98015e0f9ababa9e374fa290017
Renato <renato@Renato-MacBook-Pro.local>
2020-11-04 03:00:55 +0200

    testes execicios2

--- D01/EX02/index.js
+++ D01/EX02/index.js
@@ -1,16 +1,16 @@
 function isBiggerThan() {
-  let chair = 2 ;
-    if (chair < 4) {
-      console.log("the number ${chair} is less  to 4");
-    }
+  let chair = 1 ;
     if (chair > 4) {
-      console.log("The number ${chair} is bigger than 4")
+      return console.log("the number 3 is less than 4");
     }
-    else {
-      console.log("the number ${chair} is equal to 4")
+    if (chair < 4) {
+      return console.log("The number 5 is bigger than 4");
+    }
+    if (chair = 4) {
+      return console.log("the number 4 is equal to 4");
     }
 
   }
-
+isBiggerThan();
 // Do not remove or change this line, or the tests won't work
 export { isBiggerThan };
