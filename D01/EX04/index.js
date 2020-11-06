a1a9065581bef82e40bad94d9d4c5b51766cefde
Renato <renato@Renato-MacBook-Pro.local>
2020-11-06 19:17:03 +0200

    exe 4

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,20 +1,20 @@
-function modulo(chair, bags) {
-  chair>0
-  bags>0  
+function modulo(chair, bags,q1,q2,q3) {
+
+  
+  if (chair == bags) {
+     q3 = chair % bags
+    let bed = console.log("The number 4 is equal to 4");
+  }
   if (chair < bags) {
-    const q1= chair%bags
+    q1 = chair % bags
     let top = console.log("The number 3 is less than 5");
   }
   if (chair > bags) {
-    
-    if (q2>0) {
-    const q2= chair%bags
-    let flor = console.log("The modulo of 8 % 3 is 2");
-  }
-  if (chair == bags) {
-    const q3= chair%bags
-    let bed = console.log("The number 4 is equal to 4");
-  }
-}
+    console.log("the number 6 is bigger than 3")
+    q2 = chair % bags
+    if(chair>q2){
+    console.log("The number  8 is bigger than 3 ")
+  } else{ console.log("The number 8 is bigger than 2")}
+}}
 // Do not remove or change this line, or the tests won't work
 export { modulo };
