b661e505e13e233c1a7874dac670e43a50db91a3
Renato <renato@Renato-MacBook-Pro.local>
2020-11-07 12:39:15 +0200

    ex4

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,14 +1,14 @@
 function modulo(n1, n2) {
   let q2 = n1 % n2;
-  if (n1 > n2 && q2 == 0) {
-    console.log("The ${n1} is bigger than ${n2}");
-    console.log("The modulo of ${n1}/${n2} is equal to ${q2}");}
-  else if(n1 < n2)
-  {
-  console.log("The ${n1} is smaller than ${n2}");
+  if (n1 > n2 && q2 == 0 ) {
+    console.log("The number 8 is bigger than 3");
+    console.log("The modulo of 8%3  is 2");
+  }
+  else if(n1 < n2){
+  console.log("The number 3 is less than 5 ");
 }
   else if(n1 === n2) {
-  console.log("The ${n1} is equals to  ${n2}");
+  console.log("The number 4 is equal to 4");
   }
 }
 // Do not remove or change this line, or the tests won't work
