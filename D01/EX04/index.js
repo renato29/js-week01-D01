37fbb83fb28197630054e1d185249b8ce58d150e
Renato <renato@Renato-MacBook-Pro.local>
2020-11-07 12:12:02 +0200

    trying ex4

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,20 +1,15 @@
-function modulo(chair, bags,q1,q2,q3) {
-
-  
-  if (chair == bags) {
-     q3 = chair % bags
-    let bed = console.log("The number 4 is equal to 4");
+function modulo(n1, n2) {
+  let q2 = n1 % n2;
+  if (n1 > n2 && q2 == 0) {
+    console.log("The ${n1} is bigger than ${n2}");
+    console.log("The modulo of ${n1}/${n2} is equal to ${q2}");}
+  else if(n1 < n2)
+  {
+  console.log("The ${n1} is smaller than ${n2}");
+}
+  else if(n1 === n2) {
+  console.log("The ${n1} is equals to  ${n2}");
   }
-  if (chair < bags) {
-    q1 = chair % bags
-    let top = console.log("The number 3 is less than 5");
-  }
-  if (chair > bags) {
-    console.log("the number 6 is bigger than 3")
-    q2 = chair % bags
-    if(chair>q2){
-    console.log("The number  8 is bigger than 3 ")
-  } else{ console.log("The number 8 is bigger than 2")}
-}}
+}
 // Do not remove or change this line, or the tests won't work
 export { modulo };
